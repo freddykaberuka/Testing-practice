791ac4d5c61243bfc5969c9c02f4134a54075b67
freddykaberuka <freddykaberuka2009@gmail.com>
2022-05-19 14:03:44 +0200

    add condition

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const stringLength=(string)=>{
     string ="hello"
     console.log(string.length);
+    string.length < 1 || string.length > 10 ? console.log("valid string") : console.log("valid string"); 
 }
 stringLength();
